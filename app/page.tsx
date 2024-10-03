1e06b2e726f3d8320d71ea3f4bf009fc4ab74a1d
Daksh Joshi <dakshcodes@gmail.com>
2024-10-03 06:39:08 +0530

    app(update): default settings

--- app/page.tsx
+++ app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
             alt="Globe icon"
             width={16}
             height={16}
-          />
+          />z
           Go to nextjs.org →
         </a>
       </footer>
